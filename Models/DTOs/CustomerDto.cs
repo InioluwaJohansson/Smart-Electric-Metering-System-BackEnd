b7cd978a4a95ee368ad8bd2ad2acafec7f735f0c
Inioluwa Johansson <inioluwa.makinde10@gmail.com>
2025-03-30 00:20:54 -0400

    Added Prompt & Updated Dto's

--- Models/DTOs/CustomerDto.cs
+++ Models/DTOs/CustomerDto.cs
@@ -20,6 +20,9 @@ public class GetCustomerDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string UserName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string PictureUrl { get; set; }
     public List<GetMeterDto> getMeterDto { get; set; }
     public GetNotificationDto getNotificationDto { get; set; }
 }
